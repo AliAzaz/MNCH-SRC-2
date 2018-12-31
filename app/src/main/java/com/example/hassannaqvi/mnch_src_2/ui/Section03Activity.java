90c9f80e3951b98eaf912a3d34418fa3edd418ef
Mustufa786 <mustufa_ayub@hotmail.com>
2018-12-31 16:26:52 +0500

    Section 2 and 3 added [layout]

--- app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
+++ app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.hassannaqvi.mnch_src_2.R;
 
-public class Section2Activity extends AppCompatActivity {
+public class Section03Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section2);
+        setContentView(R.layout.activity_section03);
     }
 }
