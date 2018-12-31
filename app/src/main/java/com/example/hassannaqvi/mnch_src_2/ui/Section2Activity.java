55e9346917267e881f4925dd875ea43cd1746ecd
Mustufa786 <mustufa_ayub@hotmail.com>
2018-12-31 14:54:08 +0500

    string added

---
+++ app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section2Activity.java
@@ -0,0 +1,15 @@
+package com.example.hassannaqvi.mnch_src_2.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hassannaqvi.mnch_src_2.R;
+
+public class Section2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section2);
+    }
+}
