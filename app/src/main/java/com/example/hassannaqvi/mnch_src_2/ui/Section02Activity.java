de928f94c7115d30d0c27fbeafae96d0508b5aa4
Mustufa786 <mustufa_ayub@hotmail.com>
2019-01-01 10:16:41 +0500

    Section 2 and 3 added [layout]

--- app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section02Activity.java
+++ app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section02Activity.java
@@ -2,14 +2,52 @@ package com.example.hassannaqvi.mnch_src_2.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.hassannaqvi.mnch_src_2.R;
+import com.example.hassannaqvi.mnch_src_2.core.MainApp;
+import com.example.hassannaqvi.mnch_src_2.databinding.ActivitySection02Binding;
+
+import org.json.JSONException;
 
 public class Section02Activity extends AppCompatActivity {
+
+
+    ActivitySection02Binding bi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section02);
+    }
+
+    public void BtnContinue() {
+        if (!formValidation())
+            return;
+//        try {
+//            SaveDraft();
+//            if (UpdateDB()) {
+////                MainApp.endActivity(this, this, EndingActivity.class, true, fc_4_5);
+//            } else {
+//                Toast.makeText(this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
+//            }
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private void SaveDraft() {
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private boolean formValidation() {
+
+        return true;
     }
 }
--- app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
+++ app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
@@ -1,15 +1,49 @@
 package com.example.hassannaqvi.mnch_src_2.ui;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.hassannaqvi.mnch_src_2.R;
+import com.example.hassannaqvi.mnch_src_2.databinding.ActivitySection03Binding;
 
 public class Section03Activity extends AppCompatActivity {
+
+    ActivitySection03Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section03);
+
+        bi = DataBindingUtil.setContentView(this,R.layout.activity_section03);
+    }
+
+    public void BtnContinue() {
+        if (!formValidation())
+            return;
+//        try {
+//            SaveDraft();
+//            if (UpdateDB()) {
+////                MainApp.endActivity(this, this, EndingActivity.class, true, fc_4_5);
+//            } else {
+//                Toast.makeText(this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
+//            }
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private void SaveDraft() {
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private boolean formValidation() {
+
+        return true;
     }
 }
