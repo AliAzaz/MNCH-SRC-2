3e1a0deb7b33a5bcb6c8e7f1058233648b4cefee
Mustufa786 <mustufa_ayub@hotmail.com>
2019-01-02 12:10:49 +0500

    skip patterns done in section 3

--- app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
+++ app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 
 import com.example.hassannaqvi.mnch_src_2.R;
@@ -20,7 +21,6 @@ public class Section03Activity extends AppCompatActivity {
         bi.setCallback(this);
 
 
-
         setupViews();
     }
 
@@ -30,29 +30,91 @@ public class Section03Activity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                if( checkedId == bi.mnc03a.getId()
+                if (checkedId == bi.mnc03a.getId()
                         || checkedId == bi.mnc03b.getId()
                         || checkedId == bi.mnc03c.getId()
                         || checkedId == bi.mnc03d.getId()
                         || checkedId == bi.mnc03e.getId()
-                        || checkedId == bi.mnc03f.getId()){
+                        || checkedId == bi.mnc03f.getId()) {
 
 
+                    bi.mnc0396x.setVisibility(View.GONE);
+                    bi.mnc0396x.setText(null);
                     bi.fldgrpmnc04.setVisibility(View.GONE);
                     bi.fldgrpmnc05.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc06.setVisibility(View.GONE);
+                    bi.fldgrpmnc07.setVisibility(View.GONE);
+                    bi.fldgrpmnc08.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc09.setVisibility(View.VISIBLE);
                 }
 
-                if(checkedId == bi.mnc03g.getId()
+                if (checkedId == bi.mnc03g.getId()
                         || checkedId == bi.mnc03h.getId()
                         || checkedId == bi.mnc03i.getId()
-                        || checkedId == bi.mnc03j.getId()){
+                        ) {
+                    bi.mnc0396x.setText(null);
+                    bi.mnc0396x.setVisibility(View.GONE);
                     bi.fldgrpmnc04.setVisibility(View.VISIBLE);
                     bi.fldgrpmnc05.setVisibility(View.GONE);
+                    bi.fldgrpmnc06.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc07.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc08.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc09.setVisibility(View.VISIBLE);
+
+                }
+
+                if (checkedId == bi.mnc03j.getId()) {
+                    bi.mnc0396x.setText(null);
+                    bi.mnc0396x.setVisibility(View.GONE);
+                    bi.fldgrpmnc05.setVisibility(View.GONE);
+                    bi.fldgrpmnc06.setVisibility(View.GONE);
+                    bi.fldgrpmnc07.setVisibility(View.GONE);
+                    bi.fldgrpmnc08.setVisibility(View.GONE);
+                    bi.fldgrpmnc09.setVisibility(View.GONE);
+                }
+
+                if (checkedId == bi.mnc0397.getId()
+                        || checkedId == bi.mnc0398.getId()) {
+
+
+                    bi.mnc0396x.setVisibility(View.GONE);
+                    bi.mnc0396x.setText(null);
+                    bi.fldgrpmnc04.setVisibility(View.GONE);
+                    bi.fldgrpmnc05.setVisibility(View.GONE);
+                    bi.fldgrpmnc06.setVisibility(View.GONE);
+                    bi.fldgrpmnc07.setVisibility(View.GONE);
+                    bi.fldgrpmnc08.setVisibility(View.GONE);
+                    bi.fldgrpmnc09.setVisibility(View.GONE);
+                }
+
+                if (checkedId == bi.mnc0396.getId()) {
+                    bi.mnc0396x.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc04.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc05.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc06.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc07.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc08.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc09.setVisibility(View.VISIBLE);
                 }
 
 
             }
         });
+
+        CompoundButton.OnCheckedChangeListener check1 = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked) {
+                    bi.fldgrpmnc09.setVisibility(View.GONE);
+                } else {
+                    bi.fldgrpmnc09.setVisibility(View.VISIBLE);
+                }
+            }
+        };
+
+        bi.mnc08a.setOnCheckedChangeListener(check1);
+        bi.mnc0898.setOnCheckedChangeListener(check1);
 
     }
 
