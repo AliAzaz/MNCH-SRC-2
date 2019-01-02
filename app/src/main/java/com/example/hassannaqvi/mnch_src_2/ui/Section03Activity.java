fe3f9e9a43b78252dd81b87194d39fb31ed5b093
Mustufa786 <mustufa_ayub@hotmail.com>
2019-01-02 10:40:16 +0500

    issue fixed

--- app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
+++ app/src/main/java/com/example/hassannaqvi/mnch_src_2/ui/Section03Activity.java
@@ -3,6 +3,8 @@ package com.example.hassannaqvi.mnch_src_2.ui;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioGroup;
 
 import com.example.hassannaqvi.mnch_src_2.R;
 import com.example.hassannaqvi.mnch_src_2.databinding.ActivitySection03Binding;
@@ -14,8 +16,44 @@ public class Section03Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section03);
+        bi.setCallback(this);
+
+
+
+        setupViews();
+    }
+
+    private void setupViews() {
+
+        bi.mnc03.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if( checkedId == bi.mnc03a.getId()
+                        || checkedId == bi.mnc03b.getId()
+                        || checkedId == bi.mnc03c.getId()
+                        || checkedId == bi.mnc03d.getId()
+                        || checkedId == bi.mnc03e.getId()
+                        || checkedId == bi.mnc03f.getId()){
+
+
+                    bi.fldgrpmnc04.setVisibility(View.GONE);
+                    bi.fldgrpmnc05.setVisibility(View.VISIBLE);
+                }
+
+                if(checkedId == bi.mnc03g.getId()
+                        || checkedId == bi.mnc03h.getId()
+                        || checkedId == bi.mnc03i.getId()
+                        || checkedId == bi.mnc03j.getId()){
+                    bi.fldgrpmnc04.setVisibility(View.VISIBLE);
+                    bi.fldgrpmnc05.setVisibility(View.GONE);
+                }
+
+
+            }
+        });
+
     }
 
     public void BtnContinue() {
